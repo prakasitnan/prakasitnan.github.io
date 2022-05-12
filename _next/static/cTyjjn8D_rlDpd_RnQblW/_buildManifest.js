8ad7cd70ccfbc683039c555c715ff01e3ff461a4
B19r3d4456 <prakasit.nan@gmail.com>
2022-05-12 17:58:14 +0700

    🚀 Deploy [12/05/22]

--- _next/static/cTyjjn8D_rlDpd_RnQblW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/592-ba561a3c75aa0c52.js","static/chunks/pages/index-2c2b9d99e513baac.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
